5c523fd19b43eeec3d23f9c1666218073acff26e
Reiyirak <pdsmej@gmail.com>
2023-05-15 20:53:27 -0600

    Initial setup of user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
